1e398fc632ff028577d66909d0b440d7e6597b5c
Brian <nbmbestre91@gmail.com>
2023-08-26 16:31:48 +0800

    updated routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<Layout />}>
+				<Route path='/heilybestre/' element={<Layout />}>
 					<Route index element={<Home />} />
-					<Route path='about' element={<About />} />
+					<Route path='/heilybestre/about' element={<About />} />
 					{/* <Route path="*" element={<NoMatch />} /> */}
 				</Route>
 			</Routes>
