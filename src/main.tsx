359f2f6bb735e558e2ec8219c2c54b2769189dc8
Brian <nbmbestre91@gmail.com>
2023-08-26 16:25:39 +0800

    remove basename on BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
