b12fe9cadfdbe16c20e0b3915241d6c84589d741
Brian <nbmbestre91@gmail.com>
2023-08-26 16:14:51 +0800

    updated environement variables

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
