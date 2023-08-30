e7b1c7fb3050003dd94e3792170d781d160ef99d
Brian <nbmbestre91@gmail.com>
2023-08-30 23:58:21 +0800

    tailwind css update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Roboto'],
 				abhaya: ['"Abhaya Libre ExtraBold"'],
-				display: ['Sen'],
+				poppins: ['Poppins'],
+			},
+			colors: {
+				neutral: {
+					100: '#FFFFFF',
+					300: '#F1F3F7',
+					400: '#E1E4ED',
+					500: '#B4B9C9',
+					600: '#6D758F',
+				},
 			},
 		},
 	},
