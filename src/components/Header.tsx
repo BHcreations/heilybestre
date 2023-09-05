2040c91be50eeb41c45d5898311136b6a50a964e
Brian <nbmbestre91@gmail.com>
2023-09-06 00:40:14 +0800

    Header mobile view update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
 							leaveFrom='transform opacity-100 scale-100'
 							leaveTo='transform opacity-0 scale-95'
 						>
-							<Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50'>
+							<Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-[60]'>
 								<div className='py-1'>
 									<Menu.Item>
 										{() => (
